6ea995666af385098528631a4267ff831f066111
Maxim Lyuzin <mr.maxxe@gmail.com>
2022-02-07 15:34:46 +0300

    Add files via upload

---
+++ 8.1/producer.c
@@ -0,0 +1,59 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <amqp.h>
+#include <amqp_tcp_socket.h>
+
+#include "utils.h"
+
+int main(int argc, char const *const *argv) {
+  char const *hostname;
+  int port, status;
+  char const *exchange;
+  char const *routingkey;
+  char const *messagebody;
+  amqp_socket_t *socket = NULL;
+  amqp_connection_state_t conn;
+
+  if (argc < 6) {
+    fprintf(
+        stderr,
+        "Usage: amqp_sendstring host port exchange routingkey messagebody\n");
+    return 1;
+  }
+
+  hostname = argv[1];
+  port = atoi(argv[2]);
+  exchange = argv[3];
+  routingkey = argv[4];
+  messagebody = argv[5];
+
+  conn = amqp_new_connection();
+
+  socket = amqp_tcp_socket_new(conn);
+
+  status = amqp_socket_open(socket, hostname, port);
+
+  amqp_login(conn, "/", 0, 131072, 0, AMQP_SASL_METHOD_PLAIN,
+                               "guest", "guest");
+  amqp_channel_open(conn, 1);
+  amqp_get_rpc_reply(conn);
+
+  {
+    amqp_basic_properties_t props;
+    props._flags = AMQP_BASIC_CONTENT_TYPE_FLAG | AMQP_BASIC_DELIVERY_MODE_FLAG;
+    props.content_type = amqp_cstring_bytes("text/plain");
+    props.delivery_mode = 2; /* persistent delivery mode */
+    amqp_basic_publish(conn, 1, amqp_cstring_bytes(exchange),
+                                    amqp_cstring_bytes(routingkey), 0, 0,
+                                    &props, amqp_cstring_bytes(messagebody));
+  }
+
+  amqp_channel_close(conn, 1, AMQP_REPLY_SUCCESS);
+  amqp_connection_close(conn, AMQP_REPLY_SUCCESS);
+  amqp_destroy_connection(conn);
+  return 0;
+}
+
